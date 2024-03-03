d275fcf999e589f39d00c4770d89d85d9f2d3cfd
Rahul Kumar <rk7870382701@gmail.com>
2024-03-03 16:12:24 +0530

    factorial code added

---
+++ JavaPractice/src/codings/Factorial.java
@@ -0,0 +1,26 @@
+package codings;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		scanner.close();
+
+		if (num > 0) {
+			int fact = 1;
+			while (num > 0) {
+				fact = fact * num;
+				num--;
+			}
+			System.out.println("Factorial of " + num + " is " + fact);
+
+		} else {
+			System.out.println("Sorry mate, factorial of a negative number : " + num + " does not exist !!");
+		}
+
+	}
+
+}
